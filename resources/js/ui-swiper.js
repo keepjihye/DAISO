50d1359650df95dd47157ed0ae167c09f0dfa9cf
keepjihye <yoojihea60@gmail.com>
2024-10-21 00:38:06 +0900

    [keepjihye]swiper-pagination btn / 해결 전

---
+++ resources/js/ui-swiper.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+
+    },
+
+},
+);
