cc9c95aeb6c4ccef8ef3b06edf0864da3066308f
keepjihye <yoojihea60@gmail.com>
2024-10-21 03:40:51 +0900

    [keepjihye]스와이퍼연동까지 완료

--- resources/js/ui-swiper.js
+++ resources/js/ui-swiper.js
@@ -1,4 +1,7 @@
-var swiper = new Swiper(".swiper__main", {
+var swiperTopNum = $('.swiper__main').find('.swiper-slide');
+var swiperSubNum = $('.swiper__container').find('.swiper__container');
+
+var swiperMain = new Swiper(".swiper__main", {
     spaceBetween: 30,
     centeredSlides: true,
     // autoplay: {
@@ -16,11 +19,15 @@ var swiper = new Swiper(".swiper__main", {
 },
 );
 
-var swiper = new Swiper(".swiper__container", {
+var swiperCon = new Swiper(".swiper__container", {
     slidesPerView: 5,
     spaceBetween: 8,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    slideToClickedSlide: true,
 });
+
+swiperMain.controller.control = swiperCon;
+swiperCon.controller.control = swiperMain;
