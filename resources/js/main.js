27e01c9ae9711aecdc56ac08caa2bd5d42508d37
keepjihye <yoojihea60@gmail.com>
2024-11-05 13:59:26 +0900

    review

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,3 +9,22 @@ $(".el-collapse-item__wrap").on("click", function () {
         $wrapCon.slideDown().addClass("active");
     }
 });
+
+
+$(document).ready(function () {
+    $('.product-cont__lst a').on('click', function (e) {
+        e.preventDefault(); // 기본 앵커 클릭 동작을 방지
+
+        // 모든 <li> 요소에서 'active' 클래스 제거
+        $('.product-cont__lst li').removeClass('active');
+
+        // 클릭한 <a> 태그의 부모 <li>에 'active' 클래스 추가
+        $(this).parent('li').addClass('active');
+
+        // 대상 섹션으로 부드럽게 스크롤
+        const targetId = $(this).attr('href');
+        const targetPosition = $(targetId).offset().top;
+
+        $('html, body').animate({ scrollTop: targetPosition }, 500);
+    });
+});
